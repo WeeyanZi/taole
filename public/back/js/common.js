0723b52c506841206f6f7b2a772bd308cb0ccf76
WenYuanZhi <544268402@qq.com>
2019-08-28 17:25:38 +0800

    后台公共部分完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,11 +1,53 @@
 
-$(document).ajaxStart(function(){
+$(document).ajaxStart(function () {
     NProgress.start();
 });
 
-$(document).ajaxStop(function(){
-    setTimeout(function(){
+$(document).ajaxStop(function () {
+    setTimeout(function () {
         NProgress.done();
-    },500)
-    
+    }, 500)
+
 });
+
+
+if (location.href.indexOf('login.html') === -1) {
+    $.ajax({
+        type: 'get',
+        url: '/employee/checkRootLogin',
+        dataType: 'json',
+        success: function (info) {
+            if (info.error === 400) {
+                location.href = 'login.html'
+            }
+        }
+    })
+}
+
+
+
+
+$(function () {
+    $('#categoryManage').click(function () {
+        $(this).next().stop().slideToggle();
+    })
+
+    $('.lt_topbar .icon_menu').click(function () {
+        $('.lt_aside').toggleClass('hidemenu');
+        $('.lt_topbar').toggleClass('hidemenu');
+        $('.lt_main').toggleClass('hidemenu');
+    })
+
+    $('.lt_topbar .icon_logout').click(function () {
+        $.ajax({
+            type: 'get',
+            url: '/employee/employeeLogout',
+            dataType: 'json',
+            success: function (info) {
+                if (info.success) {
+                    location.href = 'login.html';
+                }
+            }
+        })
+    })
+})
